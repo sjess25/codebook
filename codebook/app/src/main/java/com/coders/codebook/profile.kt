c75e9ac79b10e89fc9dc888f08c291be5a150ab5
S Jessica T Lopez <tjess.uamc@gmail.com>
2019-06-03 19:29:02 -0500

    Listview con adaptador simple

--- codebook/app/src/main/java/com/coders/codebook/profile.kt
+++ codebook/app/src/main/java/com/coders/codebook/profile.kt
@@ -2,11 +2,31 @@ package com.coders.codebook
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        var miChallenges: ArrayList<String> = ArrayList()
+
+        miChallenges.add("Reto 1")
+        miChallenges.add("Reto 2")
+        miChallenges.add("Reto 3")
+        miChallenges.add("Reto 4")
+
+        val listChallenge = findViewById<ListView>(R.id.myChallengeList)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, miChallenges)
+
+        listChallenge.adapter = adapter
+
+        listChallenge.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, miChallenges.get(position), Toast.LENGTH_LONG).show()
+        }
     }
 }
