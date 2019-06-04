7d8b80fc7f34a446859256d7b78d16058d78162a
Diana Celeste Nuño Ramírez <diana.leneant@gmail.com>
2019-06-04 09:44:44 -0500

    Middleware server update.
    Registration post added, quick sql query function added.

--- server/server.js
+++ server/server.js
@@ -4,28 +4,41 @@ const server = express();
 
 const parser = require('body-parser');
 
+// misc functions
+
+function sqlQuery(sql, callback) {
+  var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'jess',
+    password: '1qaz!QAZ,.,',
+    database: 'codebook'
+  });
+
+  con.query(sql, callback);
+
+  con.end();
+}
+
 server.use(parser.urlencoded({
   extended: true
 }));
 server.use(parser.json());
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-// http://35.229.58.165:81/login?user=usuario&password=contraseña
+// http://URL:81/login?user=usuario&password=contraseña
 server.get('/login', function (req, res) {
   var usr = req.query.user;
   var pss = req.query.password;
 
-  console.log('Conexión.');
-
-  var con = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'jess',
-    password : '1qaz!QAZ,.,',
-    database : 'codebook'
-  });
-
   var sql = `SELECT Password FROM Users WHERE NickName = "${usr}" OR Email = "${usr}";`;
-  console.log(sql);
-  con.query(sql,
+
+  console.log(`Petición de login.\n${sql}`);
+
+  sqlQuery(sql,
     function (error, results, fields) {
       if (error) {
         console.error(error);
@@ -45,8 +58,41 @@ server.get('/login', function (req, res) {
           res.send('2');
         }
       }
+    });
+});
+
+server.post('/register', function (req, res) {
+  var nick = req.body.NickName;
+  var mail = req.body.Email;
+  var pass = req.body.Password;
+  var name = req.body.Name;
+
+  var sql = `INSERT INTO Users (NickName, Email, Password, Name)
+              VALUES ("${nick}", "${mail}", "${pass}", "${name}");`;
+
+  console.log(`Registrando usuario.\n${sql}`);
+
+  sqlQuery(sql, function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      res.send({
+        result: "error",
+        data: error.message
+      });
+    } else {
+      if (results.insertId) {
+        res.send({
+          result: "success",
+          data: results.insertId
+        });
+      } else {
+        res.send({
+          result: "error",
+          data: "Failed at insertion."
+        });
+      }
+    }
   });
-  con.end();
 });
 
 server.listen(81, function () {
