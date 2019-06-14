eb7b623bdc598fa0d13893ad0339651dcacf93d4
S Jessica T Lopez <tjess.uamc@gmail.com>
2019-06-14 12:33:22 -0500

    Se agrega actividad para retos del asesorado

---
+++ codebook/app/src/main/java/com/coders/codebook/myChallenge.kt
@@ -0,0 +1,12 @@
+package com.coders.codebook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class myChallenge : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_challenge)
+    }
+}
