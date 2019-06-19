0434a89d31cdfbc248d5fa22cc86e6f6c2b24fc0
Srdbqp <faq.de.system@gmail.com>
2019-06-19 15:30:38 -0500

    Merge branch 'master' of /home/dbp/Documents/UAM/5T/Laboratorio_T1/codebook/codebook with conflicts.

---
+++ codebook/app/src/main/java/com/coders/codebook/Searcher.kt
@@ -0,0 +1,12 @@
+package com.coders.codebook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Searcher : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searcher)
+    }
+}
