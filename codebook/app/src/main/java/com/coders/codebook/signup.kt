413e7634d5680f64afc88a0a5120f2e10d780c74
S Jessica T Lopez <tjess.uamc@gmail.com>
2019-06-26 15:47:36 -0500

    Se implemeta regex para la validación de las cadenas email y password

--- codebook/app/src/main/java/com/coders/codebook/signup.kt
+++ codebook/app/src/main/java/com/coders/codebook/signup.kt
@@ -10,6 +10,9 @@ import android.widget.EditText
 import android.widget.Toast
 import com.android.volley.Response
 import org.json.JSONObject
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 
 class signup : AppCompatActivity() {
 
@@ -36,44 +39,53 @@ class signup : AppCompatActivity() {
                 val email = editEmail.getText().toString()
 
                 if (nickName != "" && name != "" && password != "" && email != ""){
-                    val params = HashMap<String,String>()
-                    params["Name"] = name
-                    params["NickName"] = nickName
-                    params["Email"] = email
-                    params["Password"] = password
-                    val jsonObject = JSONObject(params)
                     val intent = Intent(this, login::class.java)
-                    Network.postHTTP(this, "http://35.231.202.82:81/register", jsonObject, Response.Listener<JSONObject>{
-                        response ->
-                        try {
-                            //val respJson: String = response.get("result").toString()
-                            //Toast.makeText(this, respJson, Toast.LENGTH_LONG).show()
-                            if (response.get("result").toString() == "success"){
-                                Toast.makeText(this, "registered user", Toast.LENGTH_LONG).show()
-                                val params_post = LinkedHashMap<String,String>()
-                                params_post["ID"] = 8.toString()
-                                params_post["Who"] = response.get("data").toString()
-                                params_post["Challenge"] = 14.toString()
-                                val jsonUser = JSONObject(params_post)
+                    if (isEmailValid(email)) {
+                        if (isPasswordValid(password)) {
+                            val params = HashMap<String,String>()
+                            params["Name"] = name
+                            params["NickName"] = nickName
+                            params["Email"] = email
+                            params["Password"] = password
+                            val jsonObject = JSONObject(params)
+                            Network.postHTTP(this, "http://35.231.202.82:81/register", jsonObject, Response.Listener<JSONObject>{
+                                    response ->
+                                try {
+                                    //val respJson: String = response.get("result").toString()
+                                    //Toast.makeText(this, respJson, Toast.LENGTH_LONG).show()
+                                    if (response.get("result").toString() == "success"){
+                                        Toast.makeText(this, "registered user", Toast.LENGTH_LONG).show()
+                                        val params_post = LinkedHashMap<String,String>()
+                                        params_post["ID"] = 8.toString()
+                                        params_post["Who"] = response.get("data").toString()
+                                        params_post["Challenge"] = 14.toString()
+                                        val jsonUser = JSONObject(params_post)
 
-                                Network.getJson(this, "http://35.231.202.82:81/data", jsonUser, Response.Listener<JSONObject>{
-                                        response_Json ->
-                                    try {
-                                        Log.d("send register", jsonUser.toString())
-                                        Log.d("response register", response_Json.toString())
-                                        (this as signup).finish()
-                                        startActivity(intent)
-                                    }catch (e: java.lang.Exception){
-                                        Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                                        Network.getJson(this, "http://35.231.202.82:81/data", jsonUser, Response.Listener<JSONObject>{
+                                                response_Json ->
+                                            try {
+                                                Log.d("send register", jsonUser.toString())
+                                                Log.d("response register", response_Json.toString())
+                                                (this as signup).finish()
+                                                startActivity(intent)
+                                            }catch (e: Exception){
+                                                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                                            }
+                                        })
+                                    } else {
+                                        Toast.makeText(this, response.get("data").toString(), Toast.LENGTH_LONG).show()
                                     }
-                                })
-                            } else {
-                                Toast.makeText(this, response.get("data").toString(), Toast.LENGTH_LONG).show()
-                            }
-                        }catch (e:Exception){
-                            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                                }catch (e:Exception){
+                                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                                }
+                            })
+                        } else {
+                            Toast.makeText(this, "password no seguro\nDebe contener mas de 8 caracteres, al menos una mayuscula, minuscula, numero y caracter especial (ej: @, #, ?, &)", Toast.LENGTH_LONG).show()
                         }
-                    })
+                    } else {
+                        Toast.makeText(this, "email no valido", Toast.LENGTH_LONG).show()
+                    }
+
                 }
             } else {
                 Toast.makeText(this, "Sin conexion", Toast.LENGTH_LONG).show()
@@ -87,5 +99,21 @@ class signup : AppCompatActivity() {
         bgmail.setOnClickListener(View.OnClickListener {
             Toast.makeText(this, "Registro por GMail", Toast.LENGTH_SHORT).show()
         })
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isPasswordValid(password: String): Boolean {
+        val pattern: Pattern
+        val matcher: Matcher
+
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+
+        pattern = Pattern.compile(PASSWORD_PATTERN)
+        matcher = pattern.matcher(password)
+
+        return matcher.matches()
     }
 }
